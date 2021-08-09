29c5a70477d57de06a5bd21b197f8bca31a66157
Sudhasrinivas Pallam <pallam.sudhasrinivas@gmail.com>
2021-08-09 22:42:48 +0530

    Javadoc for FactoryPattern

--- src/com/mycode/pract/designpattern/creational/factory/CarFactoryPattern.java
+++ src/com/mycode/pract/designpattern/creational/factory/CarFactoryPattern.java
@@ -3,6 +3,16 @@ package com.mycode.pract.designpattern.creational.factory;
 /**
  * Introduces loose coupling between the classes
  *
+ * Benefits of Factory Pattern
+ * The creation of an object precludes its reuse without significant duplication of code.
+ * The creation of an object requires access to information or resources that should not be contained within the composing class.
+ * The lifetime management of the generated objects must be centralized to ensure a consistent behavior within the application.
+ *
+ * Examples :
+ * java.sql.DriverManager#getConnection()
+ * java.net.URL#openConnection()
+ * java.lang.Class#newInstance()
+ * java.lang.Class#forName()
  */
 public class CarFactoryPattern {
     public static void main(String args[]){
